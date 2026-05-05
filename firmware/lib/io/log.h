6a7fe54185b13ed935a08c426627bf8ce19fd33f
Müslüm Barış Korkmazer <hello@babico.name.tr>
2026-05-05 08:29:46 +0300

    fix(firmware): wire io/log.h into wifi state.h, remove duplicate sendLog declarations

--- firmware/lib/io/log.h
+++ firmware/lib/io/log.h
@@ -3,5 +3,3 @@
 
 // Forward-declared logging functions (defined in serial.h)
 // Handlers can include this to emit log messages to the monitor
-void sendLog(const char* msg);
-void sendLog(const __FlashStringHelper* msg);
